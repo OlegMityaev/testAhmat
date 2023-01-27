b4fc92320942dd6567c40fcada2ec3a1a2b53793
mitya <mityaev2222@gmail.com>
2023-01-28 01:23:39 +0300

    aaa

--- venv/t.py
+++ venv/t.py
@@ -1,6 +1,28 @@
 import bs4
 import requests
+import time
+from time import monotonic
 
-dict = {'ок': 2}
-dict[2] = 17
-print(dict)
+a = {}
+i = 0
+while True:
+    if input() == 'ку':
+        i += 1
+        start_time = time.time()
+        player = f'player{i}'
+        name = input()
+        a.setdefault(player, [name, 10, start_time])
+        print(a)
+
+    t = monotonic()
+    while True:
+        if input() == 'ку':
+            if round(time.time() - a[player][2]) < 5:
+                print('wait')
+            else:
+                a[player][1] += 5
+                start_time = time.time()
+                a[player][2] = start_time
+                print('+5', a[player][1])
+
+
---
+++ venv/t2.py
@@ -0,0 +1,6 @@
+a = {}
+i = 0
+while True:
+    i += 1
+    player = f'player{i}'
+    print(player)
