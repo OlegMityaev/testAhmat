7f485441c79280f769e90b73bfc36f3a5b6108dd
mitya <mityaev2222@gmail.com>
2023-01-28 01:47:08 +0300

    почти готово

--- venv/main.py
+++ venv/main.py
@@ -30,37 +30,39 @@ def start(message):
 def d(message):
     bot.send_message(message.chat.id,'/start - начало \n /долг - долг Виктора \n /писюн \n /топ')
 
-users = {}
 
-@bot.message_handler(commands=['играть'])
-def add(message):
-    if message.from_user.username in users:
-        bot.send_message(message.chat.id, f'Ты уже играешь еблан')
-        return 0
-    name = message.from_user.username
-    x = random.randint(3, 10)
-    users[name] = x
-    bot.send_message(message.chat.id, f'Мужик ты в игре, твой кок целых {users[name]} см')
+users = {}
 
 @bot.message_handler(commands=['писюн'])
 def penis(message):
     name = message.from_user.username
-    x = random.randint(-5, 10)
-    l = users[name] + x
-    users[name] = l
-    if x >= 0:
-        bot.send_message(message.chat.id, f'{name}, твій пісюн виріс на {x} см. Тепер його довжина {l} см')
+    s_t = time.time()
+    if name in users:
+        if round(time.time() - users[name][1]) >= 60*60*24: #играл сегодня или нет
+            x = random.randint(-5, 10)
+            l = users[name][0] + x
+            users[name][0] = l
+            if x >= 0:
+                bot.send_message(message.chat.id, f'{name}, твій пісюн виріс на {x} см. Тепер його довжина {l} см')
+            else:
+                if l < 0:
+                    while l < 0:
+                        x = random.randint(-5, 10)
+                        l = users[name][0] + x
+                bot.send_message(message.chat.id, f'{name}, твій пісюн зменшився на {abs(x)} см. Тепер його довжина {l} см')
+        else:
+            bot.send_message(message.chat.id, f'{name}, ти сьогодні вже грав')
     else:
-        if l < 0:
-            while l < 0:
-                x = random.randint(-5, 10)
-                l = users[name] + x
-        bot.send_message(message.chat.id, f'{name}, твій пісюн зменшився на {abs(x)} см. Тепер його довжина {l} см')
+        x = random.randint(1, 10)
+        users.setdefault(name, []).append(x)
+        users[name].append(s_t) #доб времени прироста
+        bot.send_message(message.chat.id, f'{name} ты в игре, твой кок целых {users[name][0]} см')
+
+
 
 @bot.message_handler(commands=['долг'])
 def dolg(message):
     t = cal_t()
-
     bot.send_message(message.chat.id, f'На сегодняшний день, {t} \n Виктор должен: \n Марку - 1000 руб, Олегу - 1000 руб, Глебу - 1000 руб, Стёпе - 200 руб, Ярику - пачку чапмана с яблоком, а также взять подик в школу')
 
 
